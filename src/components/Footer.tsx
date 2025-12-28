5f1a45899a05dccf4d04421805e491eeb422ee65
rner <duhairillahred927@gmail.com>
2025-12-28 19:51:36 +0700

    hapus banyak ui dak guna

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import { Heart, Github, Linkedin, Instagram, Mail, ArrowUp } from "lucide-react";
+import { Github, Linkedin, Instagram, Mail, ArrowUp } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export const Footer = () => {
